df429f01ca9f4030ba429353741daed49313556f
avinash0408 <55944756+avinash0408@users.noreply.github.com>
2025-02-20 22:06:39 +0530

    Time: 24 ms (6.95%), Space: 109.1 MB (5%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
+    int recur(vector<int>& prices,int n,int ind,int minPrice,vector<int> &dp){
+        if(ind==n) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        minPrice = min(minPrice,prices[ind]);
+        //nosell
+        int a = recur(prices,n,ind+1,minPrice,dp);
+        int b = prices[ind]-minPrice;
+        return dp[ind]=max(a,b);
+    }
     int maxProfit(vector<int>& prices) {
-       int mProfit=0;
        int n = prices.size();
-       int mini = prices[0];
-       for(int i=1;i<n;i++){
-        int p = prices[i] - mini;
-        mProfit = max(p,mProfit);
-        mini = min(mini,prices[i]);
-       }
-       return mProfit;
+       vector<int> dp(n,-1);
+       int prof = recur(prices,n,0,INT_MAX,dp);
+       return prof;
     }
 };
