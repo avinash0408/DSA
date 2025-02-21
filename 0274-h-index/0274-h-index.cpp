448ca778f577b32d58941b92e9548443efca89e8
avinash0408 <55944756+avinash0408@users.noreply.github.com>
2025-02-21 18:44:52 +0530

    Time: 0 ms (100%), Space: 12.4 MB (40.74%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -3,13 +3,25 @@ public:
     int hIndex(vector<int>& citations) {
         int h=0;
         int n = citations.size();
-        sort(citations.begin(),citations.end());
-        for(int i=0;i<n;i++){
-            if(citations[i]>=n-i){
-                h = max(h,n-i);
+        sort(citations.begin(),citations.end(),greater<int>());
+        // for(int i=0;i<n;i++){
+        //     if(citations[i]>i){
+        //         h++;
+        //     }
+        // }
+        int l = 0;
+        int r = n-1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(citations[mid]>mid){
+                h=mid+1;
+                l=mid+1;
+            }else{
+                r = mid-1;
             }
         }
         return h;
     }
 };
 
+//0 1 3 6 5
